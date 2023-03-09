edb45f085b567fe8ac02988bb157371342ccd1bb
mr-farhaan <pesronaldo@gmail.com>
2023-03-09 14:13:04 +0530

    farhan

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import*
 
 urlpatterns=[
-    path('uhome/',Userhome.as_view(),name='uhome')
+    path('uhome/',Userhome.as_view(),name='uhome'),
+    path('profile/',Profileview.as_view(),name='profile'),
+    path('bio/',BioView.as_view(),name='bio'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import View,TemplateView,CreateView
+from .forms import BioForm
+from .models import Bio
 
 # Create your views here.
 
@@ -8,3 +11,9 @@ class Userhome(TemplateView):
 
 class Profileview(TemplateView):
     template_name="profile.html"
+
+class BioView(CreateView):
+    form_class=BioForm
+    template_name="addbio.html"
+    success_url=reverse_lazy("pro")
+    
